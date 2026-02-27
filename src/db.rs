ebe419b90485393d3495854bc0f2640698327fac
cgleber <cgleber@proton.me>
2026-02-26 20:49:01 -0500

    chore: small changes to index.html and db.rs

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,3 @@
-use petname::Generator;
-// use rand::Rng; // Trait needs to be in scope for `generate`.
 pub fn generate_access_key() -> String {
-    //const CHARSET: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
-    let mut rng = rand::thread_rng();
-    let name = petname::Petnames::default()
-        .generate(&mut rng, 3, "-")
-        .expect("no names");
-    name
+    petname::petname(3, "-").unwrap_or("unexpected-no-names".to_string())
 }
